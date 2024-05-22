77bc4a5eec5eca6cbe727233a05da972d9333749
Phong Nguyen Thai <phong.nguyenth@titancorpvn.com>
2024-05-22 16:50:50 +0700

    Fix some errors

--- Trello.Application/DTOs/User/UserDTO.cs
+++ Trello.Application/DTOs/User/UserDTO.cs
@@ -7,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace Trello.Application.DTOs.User
 {
-    public class CreateUserDTO
+    public class UserDTO
+    {
+
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
+        public string Name { get; set; } = null!;
+
+    }
+    public class CreateUserDTO : UserDTO
     {
         [Required(ErrorMessage = "Email is required")]
         [RegularExpression(@"^[\w\.-]+@[\w\.-]+\.[\w\.-]+$", ErrorMessage = "Email is invalid")]
@@ -15,14 +23,16 @@ namespace Trello.Application.DTOs.User
 
         [Required(ErrorMessage = "Password is required")]
         [MaxLength(50, ErrorMessage = "Password cannot exceed 50 characters")]
+        [MinLength(6, ErrorMessage = "Password must be more than 6 characters")]
         public string Password { get; set; } = null!;
-
-        [Required(ErrorMessage = "Name is required")]
-        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
-        public string Name { get; set; } = null!;
+    }
+    public class UpdateUserDTO : UserDTO
+    {
+        [Required]
+        public int UserId { get; set; }
 
         [Required(ErrorMessage = "Gender is required")]
         [MaxLength(50, ErrorMessage = "Gender cannot exceed 50 characters")]
-        public string Gender { get; set; } = null!;  
+        public string Gender { get; set; } = null!;
     }
 }
