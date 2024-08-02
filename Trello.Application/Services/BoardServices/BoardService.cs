82f280511d3b366a0f1f663dc08bb99a778213c8
Phong Nguyen Thai <phong.nguyenth@titancorpvn.com>
2024-08-02 16:32:36 +0700

    Create comment for all service

--- Trello.Application/Services/BoardServices/BoardService.cs
+++ Trello.Application/Services/BoardServices/BoardService.cs
@@ -43,26 +43,33 @@ namespace Trello.Application.Services.BoardServices
 
         public async Task<BoardDetail> CreateBoardAsync(BoardDTO requestBody)
         {
+            // Validate the request body
             if (requestBody == null)
                 throw new ExceptionResponse(HttpStatusCode.BadRequest, ErrorField.REQUEST_BODY, ErrorMessage.NULL_REQUEST_BODY);
 
+            // Get the current user ID
             var currentUserId = UserAuthorizationHelper.GetUserAuthorizationById(_httpContextAccessor.HttpContext);
 
+            // Map the request body to a Board entity
             var board = _mapper.Map<Board>(requestBody);
             board.Id = Guid.NewGuid();
             board.CreatedDate = DateTime.UtcNow;
             board.CreatedUser = currentUserId;
             board.IsPublic = false;
             board.IsActive = true;
+
+            // Insert the new board into the repository
             await _unitOfWork.BoardRepository.InsertAsync(board);
             await _unitOfWork.SaveChangesAsync();
 
+            // Get the admin role for the board member
             var adminRole = await _roleService.GetRoleByNameAsync(BoardMemberRoleEnum.Admin.ToString());
             if (adminRole == null)
             {
                 throw new ExceptionResponse(HttpStatusCode.BadRequest, ErrorField.ROLE_FIELD, ErrorMessage.ROLE_NOT_EXIST);
             }
 
+            // Create a new board member with the admin role
             var boardMember = new BoardMember
             {
                 Id = Guid.NewGuid(),
@@ -74,16 +81,18 @@ namespace Trello.Application.Services.BoardServices
                 CreatedUser = currentUserId
             };
 
+            // Insert the new board member into the repository
             await _unitOfWork.BoardMemberRepository.InsertAsync(boardMember);
             await _unitOfWork.SaveChangesAsync();
 
+            // Map the created board entity to a BoardDetail DTO
             var createdBoardDto = _mapper.Map<BoardDetail>(board);
             return createdBoardDto;
         }
 
         public async Task<List<BoardDetail>> GetBoardAsync()
         {
-            // Get the current user
+            // Get the current user ID
             var currentUserId = UserAuthorizationHelper.GetUserAuthorizationById(_httpContextAccessor.HttpContext);
 
             // Query to get all boards
@@ -101,12 +110,16 @@ namespace Trello.Application.Services.BoardServices
 
         public async Task<List<BoardDetail>> GetBoardByFilterAsync(string? name, bool? isPublic, bool? isActive)
         {
+            // Get the current user ID
             var currentUserId = UserAuthorizationHelper.GetUserAuthorizationById(_httpContextAccessor.HttpContext);
 
+            // Query to get all boards
             IQueryable<Board> boardsQuery = _unitOfWork.BoardRepository.GetAll();
 
+            // Filter for boards created by the current user
             boardsQuery = boardsQuery.Where(u => u.CreatedUser == currentUserId);
 
+            // Apply additional filters based on the provided parameters
             if (!string.IsNullOrEmpty(name))
             {
                 boardsQuery = boardsQuery.Where(u => u.Name.Contains(name));
@@ -122,6 +135,7 @@ namespace Trello.Application.Services.BoardServices
                 boardsQuery = boardsQuery.Where(u => u.IsActive == isActive.Value);
             }
 
+            // Map the filtered boards to the BoardDetail DTO
             List<BoardDetail> boards = await boardsQuery
                 .Select(b => _mapper.Map<BoardDetail>(b))
                 .ToListAsync();
@@ -130,8 +144,10 @@ namespace Trello.Application.Services.BoardServices
 
         public async Task<List<BoardDetail>> GetBoardsByCurrentUserMembershipAsync()
         {
+            // Get the current user ID
             var currentUserId = UserAuthorizationHelper.GetUserAuthorizationById(_httpContextAccessor.HttpContext);
 
+            // Query to get boards where the current user is a member
             IQueryable<Board> boardsQuery = _unitOfWork.BoardRepository.GetAll()
                 .Join(_unitOfWork.BoardMemberRepository.GetAll(),
                       board => board.Id,
@@ -140,8 +156,10 @@ namespace Trello.Application.Services.BoardServices
                 .Where(bb => bb.boardMember.UserId == currentUserId && bb.boardMember.IsActive)
                 .Select(bb => bb.board);
 
+            // Filter for active boards
             boardsQuery = boardsQuery.Where(u => u.IsActive);
 
+            // Map the boards to the BoardDetail DTO
             List<BoardDetail> boards = await boardsQuery
                 .Select(u => _mapper.Map<BoardDetail>(u))
                 .ToListAsync();
@@ -151,105 +169,125 @@ namespace Trello.Application.Services.BoardServices
 
         public async Task<BoardDetail> UpdateBoardAsync(Guid id, BoardDTO requestBody)
         {
+            // Get the board by ID
             var board = await GetBoardByIdAsync(id);
             if (board == null)
                 throw new ExceptionResponse(HttpStatusCode.BadRequest, ErrorField.BOARD_ID_FIELD, ErrorMessage.BOARD_NOT_EXIST);
 
+            // Check if a board with the same name already exists
             var existingBoard = await GetBoardByNameAsync(requestBody.Name);
             if (existingBoard != null)
             {
                 throw new ExceptionResponse(HttpStatusCode.BadRequest, ErrorField.BOARD_FIELD, ErrorMessage.BOARD_ALREADY_EXIST);
             }
 
+            // Get the current user ID and check if they have permission to update the board
             var currentUserId = UserAuthorizationHelper.GetUserAuthorizationById(_httpContextAccessor.HttpContext);
             if (board.CreatedUser != currentUserId)
             {
                 throw new UnauthorizedAccessException("You do not have permission to update this board");
             }
 
+            // Map the request body to the existing board entity
             board = _mapper.Map(requestBody, board);
             board.UpdatedDate = DateTime.UtcNow;
             board.UpdatedUser = currentUserId;
+
+            // Update the board in the repository
             _unitOfWork.BoardRepository.Update(board);
             await _unitOfWork.SaveChangesAsync();
 
+            // Map the updated board entity to a BoardDetail DTO
             var boardDetail = _mapper.Map<BoardDetail>(board);
             return boardDetail;
         }
 
         public async Task<BoardDetail> ChangeStatusAsync(Guid Id, bool isActive)
         {
+            // Get the board by ID
             var board = await GetBoardByIdAsync(Id);
             if (board == null)
                 throw new ExceptionResponse(HttpStatusCode.BadRequest, ErrorField.BOARD_ID_FIELD, ErrorMessage.BOARD_NOT_EXIST);
 
+            // Get the current user ID and check if they have permission to update the board
             var currentUserId = UserAuthorizationHelper.GetUserAuthorizationById(_httpContextAccessor.HttpContext);
             if (board.CreatedUser != currentUserId)
             {
                 throw new UnauthorizedAccessException("You do not have permission to update this board");
             }
 
+            // Update the status and metadata of the board
             board.UpdatedDate = DateTime.UtcNow;
             board.UpdatedUser = currentUserId;
             board.IsActive = isActive;
 
+            // Update the board in the repository
             _unitOfWork.BoardRepository.Update(board);
             await _unitOfWork.SaveChangesAsync();
 
+            // Map the updated board entity to a BoardDetail DTO
             var mappedBoard = _mapper.Map<BoardDetail>(board);
             return mappedBoard;
         }
 
         public async Task<BoardDetail> ChangeVisibilityAsync(Guid id, bool isPublic)
         {
+            // Get the board by ID
             var board = await GetBoardByIdAsync(id);
             if (board == null)
                 throw new ExceptionResponse(HttpStatusCode.BadRequest, ErrorField.BOARD_ID_FIELD, ErrorMessage.BOARD_NOT_EXIST);
 
+            // Get the current user ID and check if they have permission to update the board
             var currentUserId = UserAuthorizationHelper.GetUserAuthorizationById(_httpContextAccessor.HttpContext);
             if (board.CreatedUser != currentUserId)
             {
                 throw new UnauthorizedAccessException("You do not have permission to update this board");
             }
 
+            // Update the visibility and metadata of the board
             board.UpdatedDate = DateTime.UtcNow;
             board.UpdatedUser = currentUserId;
             board.IsPublic = isPublic;
 
+            // Update the board in the repository
             _unitOfWork.BoardRepository.Update(board);
             await _unitOfWork.SaveChangesAsync();
 
+            // Map the updated board entity to a BoardDetail DTO
             var mappedBoard = _mapper.Map<BoardDetail>(board);
             return mappedBoard;
         }
 
-
         public async Task<Board> GetBoardByNameAsync(string name)
         {
+            // Get a board by its name
             return await _unitOfWork.BoardRepository.FirstOrDefaultAsync(x => x.Name.ToLower().Equals(name.ToLower()));
         }
 
         public async Task<Board> GetBoardByIdAsync(Guid id)
         {
+            // Get a board by its ID
             return await _unitOfWork.BoardRepository.GetByIdAsync(id);
         }
 
         public async Task<Board> GetBoardByCardIdAsync(Guid cardId)
         {
+            // Get the card by its ID
             var card = await _cardService.GetCardByIdAsync(cardId);
             if (card == null)
             {
                 throw new ExceptionResponse(HttpStatusCode.BadRequest, ErrorField.CARD_FIELD, ErrorMessage.CARD_NOT_EXIST);
             }
 
+            // Get the list associated with the card
             var list = await _listService.GetListByIdAsync(card.ListId);
             if (list == null)
             {
                 throw new ExceptionResponse(HttpStatusCode.BadRequest, ErrorField.LIST_FIELD, ErrorMessage.LIST_NOT_EXIST);
             }
 
+            // Get the board associated with the list
             return await GetBoardByIdAsync(list.BoardId);
         }
-
     }
 }
--- Trello.Application/Services/ToDoServices/ToDoService.cs
+++ Trello.Application/Services/ToDoServices/ToDoService.cs
@@ -40,42 +40,50 @@ namespace Trello.Application.Services.ToDoServices
 
         public async Task<ToDoDetail> CreateToDoListAsync(CreateToDoDTO requestBody)
         {
+            // Check if the request body is null and throw an exception if it is
             if (requestBody == null)
                 throw new ExceptionResponse(HttpStatusCode.BadRequest, ErrorField.REQUEST_BODY, ErrorMessage.NULL_REQUEST_BODY);
 
+            // Check if the associated card exists
             var existingCard = await _cardService.GetCardByIdAsync(requestBody.CardId);
             if (existingCard == null)
             {
                 throw new ExceptionResponse(HttpStatusCode.BadRequest, ErrorField.CARD_FIELD, ErrorMessage.CARD_NOT_EXIST);
             }
 
+            // Get the current user ID from the HTTP context
             var currentUserId = UserAuthorizationHelper.GetUserAuthorizationById(_httpContextAccessor.HttpContext);
 
+            // Check if the current user is a board member
             var existingBoardMember = await _boardMemberService.GetBoardMemberByUserIdAsync(currentUserId);
             if (existingBoardMember == null)
             {
                 throw new ExceptionResponse(HttpStatusCode.BadRequest, ErrorField.BOARD_MEMBER_FIELD, ErrorMessage.BOARD_MEMBER_NOT_EXIST);
             }
 
+            // Map the request body to a ToDo entity and set its properties
             var todo = _mapper.Map<ToDo>(requestBody);
             todo.Id = Guid.NewGuid();
             todo.IsActive = true;
             todo.CreatedDate = DateTime.UtcNow;
             todo.CreatedUser = currentUserId;
 
+            // Insert the new ToDo into the repository and save changes
             await _unitOfWork.ToDoRepository.InsertAsync(todo);
             await _unitOfWork.SaveChangesAsync();
 
+            // Map the created ToDo to a ToDoDetail DTO and return it
             var createdToDoDto = _mapper.Map<ToDoDetail>(todo);
             return createdToDoDto;
         }
 
         public async Task<List<ToDoDetail>> GetAllToDoListAsync(Guid cardId)
         {
+            // Get all active ToDo lists associated with the specified Card ID
             IQueryable<ToDo> todoListsQuery = _unitOfWork.ToDoRepository.GetAll();
-
             todoListsQuery = todoListsQuery.Where(u => u.CardId == cardId && u.IsActive);
 
+            // Map the ToDo lists to ToDoDetail DTOs and return them
             List<ToDoDetail> todoLists = await todoListsQuery
                 .Select(u => _mapper.Map<ToDoDetail>(u))
                 .ToListAsync();
@@ -85,19 +93,23 @@ namespace Trello.Application.Services.ToDoServices
 
         public async Task<List<ToDoDetail>> GetToDoListByFilterAsync(Guid cardId, string? title, bool? isActive)
         {
+            // Get all ToDo lists associated with the specified Card ID
             IQueryable<ToDo> todoListsQuery = _unitOfWork.ToDoRepository.GetAll();
-
             todoListsQuery = todoListsQuery.Where(u => u.CardId == cardId);
 
+            // Filter ToDo lists by title if provided
             if (!string.IsNullOrEmpty(title))
             {
                 todoListsQuery = todoListsQuery.Where(u => u.Title.Contains(title));
             }
+
+            // Filter ToDo lists by active status if provided
             if (isActive.HasValue)
             {
                 todoListsQuery = todoListsQuery.Where(u => u.IsActive == isActive.Value);
             }
 
+            // Map the ToDo lists to ToDoDetail DTOs and return them
             List<ToDoDetail> todoLists = await todoListsQuery
                 .Select(u => _mapper.Map<ToDoDetail>(u))
                 .ToListAsync();
@@ -107,43 +119,55 @@ namespace Trello.Application.Services.ToDoServices
 
         public async Task<ToDoDetail> UpdateToDoListAsync(Guid id, ToDoDTO requestBody)
         {
+            // Get the ToDo by ID and throw an exception if it doesn't exist
             var todo = await _unitOfWork.ToDoRepository.GetByIdAsync(id)
                 ?? throw new ExceptionResponse(HttpStatusCode.BadRequest, ErrorField.TODO_FIELD, ErrorMessage.TODO_NOT_EXIST);
 
+            // Get the current user ID from the HTTP context
             var currentUserId = UserAuthorizationHelper.GetUserAuthorizationById(_httpContextAccessor.HttpContext);
 
+            // Update the ToDo properties
             todo.UpdatedDate = DateTime.UtcNow;
             todo.UpdatedUser = currentUserId;
             todo.Title = requestBody.Title;
 
+            // Update the ToDo in the repository and save changes
             _unitOfWork.ToDoRepository.Update(todo);
             await _unitOfWork.SaveChangesAsync();
 
+            // Map the updated ToDo to a ToDoDetail DTO and return it
             var todoDetail = _mapper.Map<ToDoDetail>(todo);
             return todoDetail;
         }
 
         public async Task<ToDoDetail> ChangeStatusAsync(Guid Id, bool isActive)
         {
+            // Get the ToDo by ID and throw an exception if it doesn't exist
             var todo = await _unitOfWork.ToDoRepository.GetByIdAsync(Id)
                 ?? throw new ExceptionResponse(HttpStatusCode.BadRequest, ErrorField.TODO_FIELD, ErrorMessage.TODO_NOT_EXIST);
 
+            // Get the current user ID from the HTTP context
             var currentUserId = UserAuthorizationHelper.GetUserAuthorizationById(_httpContextAccessor.HttpContext);
 
+            // Update the ToDo's active status and metadata
             todo.UpdatedDate = DateTime.UtcNow;
             todo.UpdatedUser = currentUserId;
             todo.IsActive = isActive;
 
+            // Update the ToDo in the repository and save changes
             _unitOfWork.ToDoRepository.Update(todo);
             await _unitOfWork.SaveChangesAsync();
 
+            // Map the updated ToDo to a ToDoDetail DTO and return it
             var mappedList = _mapper.Map<ToDoDetail>(todo);
             return mappedList;
         }
 
         public async Task<ToDo> GetTodoListByIdAsync(Guid todoId)
         {
+            // Get the ToDo by ID
             return await _unitOfWork.ToDoRepository.GetByIdAsync(todoId);
         }
+
     }
 }
